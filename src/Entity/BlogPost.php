a47894a1533228b2bb6bd86ad4eec76822673359
Hasanta Sumudupriya <hsumudupriya@gmail.com>
2024-08-23 01:18:26 +0530

    BlogPost entity is made an ApiResource.

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\BlogPostRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BlogPostRepository::class)]
+#[ApiResource]
 class BlogPost
 {
     #[ORM\Id]
