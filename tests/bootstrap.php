3ba0c74b99c8597aa7e09f6f7211e256880dbe60
Hasanta Sumudupriya <hsumudupriya@gmail.com>
2024-08-31 10:59:23 +0530

    tests/bootstrap.php is updated with cache:clear.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,19 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+// executes the "php bin/console cache:clear" command
+passthru(sprintf(
+    'APP_ENV=%s php "%s/../bin/console" cache:clear --no-warmup',
+    $_ENV['APP_ENV'],
+    __DIR__
+));
