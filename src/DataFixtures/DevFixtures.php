261cd039193888e166722530f465e7b8678bfbcb
Hasanta Sumudupriya <hsumudupriya@gmail.com>
2024-08-31 11:00:35 +0530

    Logic to setup the dev env is moved from AppFixtures to DevFixtures.

---
+++ src/DataFixtures/DevFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\BlogPostFactory;
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class DevFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // create a test user for the API documentation
+        UserFactory::findOrCreate([
+            "email" => "test@api.com",
+            "password" => "abc123",
+        ]);
+        // create test blog posts.
+        BlogPostFactory::createMany(number: 40);
+    }
+}
